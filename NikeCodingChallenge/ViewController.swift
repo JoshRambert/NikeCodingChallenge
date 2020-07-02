7cadc9c64bd1f0ed74849152b7269239a3a6a741
Joshua Rambert <joshua_rambert@yahoo.com>
2020-07-02 09:56:16 -0700

    first commit

---
+++ NikeCodingChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NikeCodingChallenge
+//
+//  Created by Joshua  Rambert on 7/2/20.
+//  Copyright © 2020 Joshua Rambert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
